6ac30e377a994c5bd7f00d27a3c902dbca2269f1
Paulo Roberto Nunes de Menezes <paulo.menezes@trusthub.com.br>
2018-11-22 11:13:46 -0200

    Crud candidato

--- src/main/java/br/com/fatec/controllers/CandidatoController.java
+++ src/main/java/br/com/fatec/controllers/CandidatoController.java
@@ -1,17 +1,15 @@
 package br.com.fatec.controllers;
 
-import java.util.List;
-
+import br.com.fatec.model.Candidato;
+import br.com.fatec.services.CandidatoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import br.com.fatec.model.Candidato;
-import br.com.fatec.services.CandidatoService;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CandidatoController {
@@ -27,13 +25,42 @@ public class CandidatoController {
 	@PostMapping(value = "/cadastrar-candidato", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public String cadastroCandidato(Candidato candidato) {
 		this.service.cadastrar(candidato);
-		return "redirect:listar-candidatos";
+		return "redirect:/candidatos";
 	}
 	
-	@GetMapping("/listar-candidatos")
-	public String listarCandidatos(ModelMap model) {
-		List<Candidato> candidatos = this.service.findAll();
-		model.put("candidatos", candidatos);
-		return "listar-candidatos";
+	@GetMapping("/candidatos")
+	public ModelAndView listarCandidatos() {
+		ModelAndView modelAndView = new ModelAndView("listar-candidatos");
+		modelAndView.addObject("candidatos", this.service.findAll());
+		return modelAndView;
+	}
+
+	@GetMapping("/candidatos/{candidatoId}")
+	public String buscarCandidato(@PathVariable Long candidatoId, ModelMap model) {
+		service.findById(candidatoId).map(candidato -> {
+			model.put("candidato", candidato);
+			return model;
+		}).orElse(null);
+		return "cadastro/editar-candidato";
+	}
+
+	@PostMapping("/candidatos/{candidatoId}")
+	public String editarCandidato(@PathVariable Long candidatoId, Candidato candidatoReq) {
+		service.findById(candidatoId).map(candidato -> {
+			candidato.setNome(candidatoReq.getNome());
+			candidato.setPartido(candidatoReq.getPartido());
+			candidato.setSigla(candidatoReq.getSigla());
+			return service.cadastrar(candidato);
+		}).orElse(null);
+		return "redirect:/candidatos";
+	}
+
+	@PostMapping("/candidatos/{candidatoId}/excluir")
+	public String removerCandidato(@PathVariable Long candidatoId) {
+		service.findById(candidatoId).map(candidato -> {
+			service.delete(candidato);
+			return candidato;
+		}).orElse(null);
+		return "redirect:/candidatos";
 	}
 }
--- src/main/java/br/com/fatec/services/CandidatoService.java
+++ src/main/java/br/com/fatec/services/CandidatoService.java
@@ -1,6 +1,7 @@
 package br.com.fatec.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,19 @@ public class CandidatoService {
 	@Autowired
 	private CandidatoRepository repository;
 	
-	public void cadastrar(Candidato candidato) {
-		this.repository.save(candidato);
+	public Candidato cadastrar(Candidato candidato) {
+		return this.repository.save(candidato);
 	}
 	
 	public List<Candidato> findAll() {
 		return this.repository.findAll();
+	}
+
+	public Optional<Candidato> findById(Long candidatoId) {
+		return repository.findById(candidatoId);
+	}
+
+	public void delete(Candidato candidato) {
+		repository.delete(candidato);
 	}
 }
